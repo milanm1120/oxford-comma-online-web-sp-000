e9eb6ce49c8f2ccc48489f62cce798eb3be4cb18
Milan Mody <milanm1120@gmail.com>
2021-01-18 01:50:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  # elsif array.length == 3
+  #   first_two = array.take(2)
+  #   first_two = first_two.join(", ")
+  #   first_two << ", and " << array[2]
+  elsif array.length >= 3
+    except_last = array[0..-2]
+    except_last = except_last.join(", ")
+    except_last << ", and " << array[-1]
+  else
+    array.join
+  end
 end
