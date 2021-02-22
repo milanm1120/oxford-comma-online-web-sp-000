89afdecb565e36894984964233fd86524643a4d3
Milan Mody <milanm1120@gmail.com>
2021-02-22 21:40:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,10 @@
-def oxford_comma(array)
-  if array.length == 2
-    array.join(" and ")
-  # elsif array.length == 3
-  #   first_two = array.take(2)
-  #   first_two = first_two.join(", ")
-  #   first_two << ", and " << array[2]
-  elsif array.length >= 3
-    except_last = array[0..-2]
-    except_last = except_last.join(", ")
-    except_last << ", and " << array[-1]
-  else
-    array.join
-  end
+def oxford_comma(fruit)
+  case fruit.length
+    when 1
+      return "#{fruit[0]}"
+    when 2
+      return fruit.join(" and ")
+    else
+      return fruit[0..-2].join(", ") << ", and #{fruit[-1]}"
+    end
 end
