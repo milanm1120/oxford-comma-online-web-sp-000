4093152a31e305da32ae747000d1a9acfb3d2062
Milan Mody <milanm1120@gmail.com>
2021-02-22 21:58:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,11 @@ def oxford_comma(array)
   when 1
     return "#{array[0]}"
   when 2
-    return "#{array[0]} and #{array[1]}"
+    return array[0..1].join("and")
+  when 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    return "#{array[0]}"
   end
 end
 
