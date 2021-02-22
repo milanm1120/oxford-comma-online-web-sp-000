afb45ad6879316adc059634f0b9fa88a19a8e13e
Milan Mody <milanm1120@gmail.com>
2021-02-22 22:05:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   when 2
     return array[0..1].join(" and ")
   else
-    return array [0..-2].join (",") << "and #{array[-1]}"
+    return array [0..-2].join (", ") <<", and #{array[-1]}"
   end
 end
 
