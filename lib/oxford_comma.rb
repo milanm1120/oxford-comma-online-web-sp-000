9761b64b73af110a4dffdf143032a393f63a479b
Milan Mody <milanm1120@gmail.com>
2021-02-22 21:53:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,10 @@ def oxford_comma(array)
   case array.length
   when 1
     return "#{array[0]}"
+  when 2
+    return "#{array[0]} and #{array[1]}"
   end
 end
 
 print oxford_comma(["kiwi"])
+print oxford_comma(["kiwi", "apple"])
